81f1754281ab1dff4459330e38c21bd73081490a
Raphael Audet <raphael.audet@mendix.com>
2017-01-23 11:02:36 +0100

    3 fix travis needs longer timeout
    Signed-off-by: Raphael Audet <raphael.audet@mendix.com>

--- test.sh
+++ test.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-set -eu
+set -u
 
 make get-sample
 make build-image
 make run-container &
-TIMEOUT=30
+TIMEOUT=180
 sleep $TIMEOUT
 curl http://localhost:8080 | grep "<title>Mendix</title>"
 RETURN_CODE=$?
 if [ $RETURN_CODE -eq "0" ]; then
   echo "test.sh [SUCCES] App is reachable"
-  docker-compose down
+  # docker-compose down
   exit 0
 else
   echo "test.sh [FAILED] App is not reachable in timeout delay $TIMEOUT"
-  docker-compose down
+  # docker-compose down
   exit $RETURN_CODE
 fi
