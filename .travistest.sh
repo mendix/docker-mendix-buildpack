bd0e3cdae80c0e7316481285e939aa95321d315a
cwesdorp <chriswesdorp@gmail.com>
2017-12-04 12:12:39 +0100

    Disable SQL Server tests for docker
    SQL Server image has expired trial period and we need to make a new one. Unfortunately there are no docker images with SQL Server that automatically create a new database.

--- .travistest.sh
+++ .travistest.sh
@@ -5,5 +5,5 @@ docker-compose version
 make get-sample
 make build-image
 tests/test-generic.sh tests/docker-compose-postgres.yml
-tests/test-generic.sh tests/docker-compose-sqlserver.yml
-tests/test-generic.sh tests/docker-compose-azuresql.yml
+#tests/test-generic.sh tests/docker-compose-sqlserver.yml
+#tests/test-generic.sh tests/docker-compose-azuresql.yml
